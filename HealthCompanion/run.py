706a812330056953d111bb4f1be9cab5003057ec
Nikhil Kumar Jha <cozy@mail.uni-paderborn.de>
2020-05-24 00:25:10 +0200

    Fixing policy bug

--- HealthCompanion/run.py
+++ HealthCompanion/run.py
@@ -6,7 +6,7 @@ from typing import Text
 import os
 import rasa.utils.io
 import rasa.train
-from .RestaurantPolicy import RestaurantPolicy
+from .policy import HealthCompanionPolicy
 from rasa.core.agent import Agent
 from rasa.core.policies.memoization import MemoizationPolicy
 from rasa.core.policies.mapping_policy import MappingPolicy
